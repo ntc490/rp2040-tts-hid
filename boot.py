9cdacb13c4b99d3c2651fe5e7fb55dcbd4208dbf
Nathan Crapo <1585130+ntc490@users.noreply.github.com>
2025-12-09 23:43:57 -0700

    Only export storage if 'usr' button is pressed

--- boot.py
+++ boot.py
@@ -1,6 +1,12 @@
 import usb_cdc
 import storage
+import board
+import digitalio
 
 usb_cdc.enable(console=True, data=True)
-# Switch this based on Button status in the future
-storage.enable_usb_drive()
+
+# Export a storage device if the 'usr' button is pressed
+usr = digitalio.DigitalInOut(board.GP24)
+usr.switch_to_input(pull=digitalio.Pull.UP)
+if not usr.value:
+    storage.enable_usb_drive()
